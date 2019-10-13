0bcb3611e1553bc3b258f7df19ad229303400444
avalero <alberto.valero@bq.com>
2019-10-13 14:20:21 +0200

    rickymorty

---
+++ projects/rickymorty-graphql/src/app.js
@@ -0,0 +1,29 @@
+import request from 'request';
+
+// rickymorty entry point
+const url = 'https://rickandmortyapi.com/api/character/';
+
+/**
+ * Main App
+ * @param data all rickyandmorty database
+ */
+const runApp = data => {
+  data.forEach(element => {
+    console.log(`${element.id}: ${element.name}`);
+  });
+};
+
+const fetchData = (url, data) => {
+  if (!data) data = [];
+  console.log('fechting data...');
+  request({ url, json: true }, (error, response) => {
+    if (response.body) {
+      data = [...data, ...response.body.results];
+    }
+    if (response.body.info.next !== '')
+      fetchData(response.body.info.next, data);
+    else runApp(data);
+  });
+};
+
+fetchData(url);
