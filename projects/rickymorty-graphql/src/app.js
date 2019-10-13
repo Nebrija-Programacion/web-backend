21deaf5021dc32b1eb8269688d60dfb46cb17308
avalero <alberto.valero@bq.com>
2019-10-13 17:00:12 +0200

    graphql server is up

--- projects/rickymorty-graphql/src/app.js
+++ projects/rickymorty-graphql/src/app.js
@@ -1,4 +1,5 @@
 import { fetchData } from './fetchdata';
+import { GraphQLServer } from 'graphql-yoga';
 
 // rickymorty entry point
 const url = 'https://rickandmortyapi.com/api/character/';
@@ -7,11 +8,39 @@ const url = 'https://rickandmortyapi.com/api/character/';
  * Main App
  * @param data all rickyandmorty database
  */
-const runApp = data => {
+const runServer = (data, typeDefs, resolvers) => {
   data.forEach(element => {
     console.log(`${element.id}: ${element.name}`);
   });
+
+  const server = new GraphQLServer({
+    typeDefs,
+    resolvers,
+  });
+
+  server.start(() => {
+    console.log('The server is up!');
+  });
+};
+
+const main = data => {
+  // Type definions (schema)
+  const typeDefs = `
+    type Query {
+      hello: String!
+    }
+  `;
+
+  // Resolvers
+  const resolvers = {
+    Query: {
+      hello() {
+        return 'Hello world!';
+      },
+    },
+  };
+  return runServer(data, typeDefs, resolvers);
 };
 
 // main program
-fetchData(runApp, url);
+fetchData(main, url);
--- projects/rickymorty-graphql/src/fetchdata.js
+++ projects/rickymorty-graphql/src/fetchdata.js
@@ -1,15 +1,20 @@
 import request from 'request';
 
 const fetchData = (callback, url, data) => {
-  if (!data) data = [];
-  console.log('fechting data...');
+  if (!data) {
+    console.log('Start fetching data...');
+    data = [];
+  }
   request({ url, json: true }, (error, response) => {
     if (response.body) {
       data = [...data, ...response.body.results];
     }
     if (response.body.info.next !== '')
       fetchData(callback, response.body.info.next, data);
-    else callback(data);
+    else {
+      console.log('Done!');
+      callback(data);
+    }
   });
 };
 
