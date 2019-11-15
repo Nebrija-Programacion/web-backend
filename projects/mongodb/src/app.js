b4f0bf26da6e9ca5b4a220be690ea4a0ad4a5a23
avalero <alberto.valero@bq.com>
2019-11-15 17:11:33 +0100

    mongo db with graphql

--- projects/mongodb/src/app.js
+++ projects/mongodb/src/app.js
@@ -1,55 +1,94 @@
-import { MongoClient, ObjectID } from "mongodb";
+import { MongoClient } from "mongodb";
+import { GraphQLServer } from "graphql-yoga";
+
 import "babel-polyfill";
 
-const uri =
-  "mongodb+srv://avalero:123456abc@cluster0-e8ug9.mongodb.net/test?retryWrites=true&w=majority";
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+const usr = "avalero";
+const pwd = "123456abc";
+const url = "cluster0-e8ug9.mongodb.net/test?retryWrites=true&w=majority";
 
-const databaseName = "blog";
-const id = new ObjectID();
-console.log(id);
-console.log(id.getTimestamp());
+/**
+ * Connects to MongoDB Server and returns connected client
+ * @param {string} usr MongoDB Server user
+ * @param {string} pwd MongoDB Server pwd
+ * @param {string} url MongoDB Server url
+ */
+const connectToDb = async function(usr, pwd, url) {
+  const uri = `mongodb+srv://${usr}:${pwd}@${url}`;
+  const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
 
-client.connect(async err => {
-  if (err) {
-    return console.log(`Error connecting to ${uri}`);
+  await client.connect();
+  return client;
+};
+
+/**
+ * Starts GraphQL server, with MongoDB Client in context Object
+ * @param {client: MongoClinet} context The context for GraphQL Server -> MongoDB Client
+ */
+const runGraphQLServer = function(context) {
+  const typeDefs = `
+    type Query{
+      ok: Boolean!
+    }
+
+    type Mutation{
+      addAuthor(name: String!, age: Int!):Author!
+    }
+
+    type Author{
+      id: ID!
+      name: String!
+      age: Int!
+    }
+    `;
+
+  const resolvers = {
+    Query: {
+      ok: () => true
+    },
+
+    Mutation: {
+      addAuthor: async (parent, args, ctx, info) => {
+        const { name, age } = args;
+        const { client } = ctx;
+
+        const db = client.db("blog");
+        const collection = db.collection("authors");
+        const result = await collection.insertOne({ name, age });
+
+        return {
+          name,
+          age,
+          id: result.ops[0]._id
+        };
+      }
+    }
+  };
+
+  const server = new GraphQLServer({ typeDefs, resolvers, context });
+  const options = {
+    port: 4000
+  };
+
+  try {
+    server.start(options, ({ port }) =>
+      console.log(
+        `Server started, listening on port ${port} for incoming requests.`
+      )
+    );
+  } catch (e) {
+    console.info(e);
   }
-  console.log("Connected!");
-  const db = client.db(databaseName);
-  const collection = db.collection("authors");
+};
 
-  const result = await collection.insertOne({
-    name: "Alberto",
-    age: 41
-  });
+const runApp = async function() {
+  const client = await connectToDb(usr, pwd, url);
+  console.log("Connect to Mongo DB");
 
-  console.log(result.ops);
+  runGraphQLServer({ client });
+};
 
-  const _id = result.ops._id;
-
-  const result2 = await collection.insertOne({
-    name: "Pedro",
-    age: 41
-  });
-
-  const updateOne = await collection.updateOne(
-    { _id },
-    { $set: { name: "Andres" } }
-  );
-  console.log(`update result: ${updateOne.result.ok}`);
-
-  const updateMany = await collection.updateMany(
-    { name: "Alberto" },
-    { $set: { name: "Luis" } }
-  );
-
-  console.log(`update many: ${updateMany.result.nModified}`);
-
-  const remove = collection.deleteMany({ name: "Luis" });
-
-  // perform actions on the collection object
-  client.close();
-});
+runApp();
