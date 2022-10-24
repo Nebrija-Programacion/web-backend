f1d16394d96da2476e5349f110108a18fa673e3a
Alberto Valero <alberto.valero@bq.com>
2022-10-24 09:38:43 +0200

    mongo oak example

--- ejemplos/mongo-oak/main.ts
+++ ejemplos/mongo-oak/main.ts
@@ -14,9 +14,15 @@ type Book = {
 
 type BookSchema = Omit<Book, "id"> & { _id: ObjectId };
 
+const mongo_usr = "avalero";
+const mongo_pwd = "xxxxxxxxxxxx";
+const db_name = "bookstore";
+const mongo_url = `mongodb+srv://${mongo_usr}:${mongo_pwd}@cluster-nebrija.gcxdd.gcp.mongodb.net/${db_name}?authMechanism=SCRAM-SHA-1`;
+
 const client = new MongoClient();
-await client.connect("mongodb://127.0.0.1:27017");
-const db = client.database("bookstore");
+await client.connect(mongo_url);
+const db = client.database(db_name);
+console.log("Connected to DB");
 
 const router = new Router();
 
@@ -27,9 +33,10 @@ router
       const books = await db
         .collection<BookSchema>("books")
         .find({})
-        .sort({ title: -1 });
+        .sort({ title: -1 })
+        .toArray();
       context.response.body = books.map((book) => ({
-        id: book._id,
+        id: book._id.toString(),
         title: book.title,
         auhtor: book.author,
       }));
@@ -38,27 +45,29 @@ router
       const books = await db
         .collection<BookSchema>("books")
         .find({})
-        .sort({ title: 1 });
+        .sort({ title: 1 })
+        .toArray();
       context.response.body = books.map((book) => ({
-        id: book._id,
+        id: book._id.toString(),
         title: book.title,
         auhtor: book.author,
       }));
     }
 
-    const books = await db.collection<BookSchema>("books").find({});
+    const books = await db.collection<BookSchema>("books").find({}).toArray();
     context.response.body = books.map((book) => ({
-      id: book._id,
+      id: book._id.toString(),
       title: book.title,
       auhtor: book.author,
     }));
-    context.response.body = books;
   })
-  .get("/books/:id", (context) => {
+  .get("/books/:id", async (context) => {
     if (context.params?.id) {
-      const book: Book | undefined = books.find(
-        (book) => book.id === context.params.id
-      );
+      const book: BookSchema | undefined = await db
+        .collection<BookSchema>("books")
+        .findOne({
+          _id: new ObjectId(context.params.id),
+        });
 
       if (book) {
         context.response.body = book;
@@ -75,23 +84,59 @@ router
       context.response.status = 400;
       return;
     }
-    const book: Book = {
-      id: new Date().toISOString(),
+    const book: Partial<Book> = {
       title: value.title,
       author: value.author,
     };
-    books.push(book);
+    const id = await db
+      .collection<BookSchema>("books")
+      .insertOne(book as BookSchema);
+    book.id = id.toString();
+    context.response.body = book;
   })
-  .delete("/books/:id", (context) => {
-    if (
-      context.params?.id &&
-      books.find((book) => book.id === context.params.id)
-    ) {
-      books = books.filter((book) => book.id !== context.params.id);
-      context.response.status = 200;
-      return;
+  .delete("/books/:id", async (context) => {
+    if (context.params?.id) {
+      const count = await db.collection<BookSchema>("books").deleteOne({
+        _id: new ObjectId(context.params.id),
+      });
+      if (count) {
+        context.response.status = 200;
+      } else {
+        context.response.status = 404;
+      }
     }
-    context.response.status = 404;
+  })
+  .put("/books/:id", async (context) => {
+    if (context.params?.id) {
+      const result = context.request.body({ type: "json" });
+      const value = await result.value;
+      if (!value?.title && !value?.author) {
+        context.response.status = 400;
+        return;
+      }
+      const count = await db.collection<BookSchema>("books").updateOne(
+        { _id: new ObjectId(context.params.id) },
+        {
+          $set: {
+            title: value.title,
+            author: value.author,
+          },
+        }
+      );
+      if (count) {
+        const book = await db.collection<BookSchema>("books").findOne({
+          _id: new ObjectId(context.params.id),
+        });
+        context.response.body = {
+          id: book?._id.toString(),
+          title: book?.title,
+          author: book?.author,
+        };
+        context.response.status = 200;
+      } else {
+        context.response.status = 404;
+      }
+    }
   });
 
 const app = new Application();
