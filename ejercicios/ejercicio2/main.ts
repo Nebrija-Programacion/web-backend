1d93ba6a8e9403969ce0214343fe1d99b079818b
Alberto Valero <alberto.valero@bq.com>
2022-10-26 21:17:09 +0200

    enunciado

--- ejercicios/ejercicio2/main.ts
+++ ejercicios/ejercicio2/main.ts
@@ -1,9 +1,10 @@
-import { getQuery } from "https://deno.land/x/oak@v11.1.0/helpers.ts";
-import {
-  Application,
-  Router,
-  helpers,
-} from "https://deno.land/x/oak@v11.1.0/mod.ts";
+// Se pide realizar una API usando Deno y OAK que permita consultar una base de datos de libros. Para obtener los libros se usará la API: https://gutendex.com/
+// Debe tener los siguientes endpoints
+// GET - /books -> Devuelve un array de libros de la página 1 con los campos "id" y "titulo"
+// GET - /books/:page -> Devuelve un array de libros de la página correspondiente con los campos "id" y "titulo"
+// GET - /book/:id -> Devuelve los detalles un libro de id determinado -> devuelve "id", "titulo", array de "autores" con todos sus datos.
+
+import { Application, Router } from "https://deno.land/x/oak@v11.1.0/mod.ts";
 
 type Author = {
   name: string;
