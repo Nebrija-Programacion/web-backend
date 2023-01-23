d1bf54c08251b3a95453d584c5d5f00a056c1ab1
Alberto Valero Gómez <avalero@users.noreply.github.com>
2023-01-23 15:59:38 +0100

    Update query.ts

--- examenes/2223/ordinaria/src/resolvers/query.ts
+++ examenes/2223/ordinaria/src/resolvers/query.ts
@@ -16,7 +16,7 @@ export const Query = {
     args: { ids: string[] }
   ): Promise<Array<CharacterAPIRest>> => {
     const { ids } = args;
-    const characters = wait fetch(`https://rickandmortyapi.com/api/character/${args.ids.toString()}`);
+    const characters = wait fetch(`https://rickandmortyapi.com/api/character/${ids.toString()}`);
     return characters.json();
   },
 };
