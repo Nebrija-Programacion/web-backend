dfb452b115849ea7e1838f051485fb846bdf6054
Alberto Valero <alberto.valero@bq.com>
2023-10-09 17:31:22 +0200

    country code check

--- ejemplos/apirest/main.ts
+++ ejemplos/apirest/main.ts
@@ -13,12 +13,17 @@ app
     async (req: Request, res: Response) => {
       try {
         const zipcode = req.params.zipcode;
+        const countrycode = req.params.countrycode;
         if (isNaN(Number(zipcode))) {
           res.status(400).send("Zipcode must be a number");
           return;
         }
 
-        const countrycode = req.params.countrycode;
+        if (countrycode.length !== 2) {
+          res.status(400).send("Country code must be 2 characters");
+          return;
+        }
+
         const location = await getLocation(zipcode, countrycode);
         res.status(200).send(location);
       } catch (e) {
@@ -35,6 +40,11 @@ app
 
         if (isNaN(Number(zipcode))) {
           res.status(400).send("Zipcode must be a number");
+          return;
+        }
+
+        if (countrycode.length !== 2) {
+          res.status(400).send("Country code must be 2 characters");
           return;
         }
 
