3fff14d5d1599b9fe84fe5d3f18b2ae8b18a05c8
Alberto Valero Gómez <alberto.valero@bqeducacion.cc>
2024-11-05 16:46:50 +0100

    lo subo para que Rafa esté tranquilo

--- ejemplos/api_mongo/main.ts
+++ ejemplos/api_mongo/main.ts
@@ -1,6 +1,6 @@
-import { MongoClient } from "mongodb";
+import { MongoClient, ObjectId } from "mongodb";
 import type { BookModel, UserModel } from "./types.ts";
-import { fromModelToUser } from "./utils.ts";
+import { fromModelToBook, fromModelToUser } from "./utils.ts";
 
 const MONGO_URL = Deno.env.get("MONGO_URL");
 if (!MONGO_URL) {
@@ -31,13 +31,13 @@ const handler = async (req: Request): Promise<Response> => {
           usersDB.map((u) => fromModelToUser(u, booksCollection))
         );
         return new Response(JSON.stringify(users));
+      } else {
+        const usersDB = await usersCollection.find().toArray();
+        const users = await Promise.all(
+          usersDB.map((u) => fromModelToUser(u, booksCollection))
+        );
+        return new Response(JSON.stringify(users));
       }
-
-      const usersDB = await usersCollection.find().toArray();
-      const users = await Promise.all(
-        usersDB.map((u) => fromModelToUser(u, booksCollection))
-      );
-      return new Response(JSON.stringify(users));
     } else if (path === "/user") {
       const email = url.searchParams.get("email");
       if (!email) return new Response("Bad request", { status: 400 });
@@ -47,6 +47,18 @@ const handler = async (req: Request): Promise<Response> => {
       if (!userDB) return new Response("User not found", { status: 404 });
       const user = await fromModelToUser(userDB, booksCollection);
       return new Response(JSON.stringify(user));
+    } else if (path === "/books") {
+      const booksDB = await booksCollection.find().toArray();
+      const books = booksDB.map((b) => fromModelToBook(b));
+      return new Response(JSON.stringify(books));
+    } else if (path === "/book") {
+      // /book?id=1232312131233223232
+      const id = url.searchParams.get("id");
+      if (!id) return new Response("Bad request", { status: 400 });
+      const bookDB = await booksCollection.findOne({ _id: new ObjectId(id) });
+      if (!bookDB) return new Response("Book not found", { status: 404 });
+      const book = fromModelToBook(bookDB);
+      return new Response(JSON.stringify(book));
     }
   } else if (method === "POST") {
     if (path === "/user") {
@@ -66,32 +78,81 @@ const handler = async (req: Request): Promise<Response> => {
         age: user.age,
         books: [],
       });
+
       return new Response(
         JSON.stringify({
           name: user.name,
           email: user.email,
           age: user.age,
+          books: [],
+          id: insertedId,
+        }),
+        { status: 201 }
+      );
+    } else if (path === "/book") {
+      const book = await req.json();
+      if (!book.title || !book.pages) {
+        return new Response("Bad request", { status: 400 });
+      }
+      const { insertedId } = await booksCollection.insertOne({
+        title: book.title,
+        pages: book.pages,
+      });
+      return new Response(
+        JSON.stringify({
+          title: book.title,
+          pages: book.pages,
           id: insertedId,
         }),
         { status: 201 }
       );
     }
   } else if (method === "PUT") {
-    const user = await req.json();
-    if (!user.name || !user.email || !user.age) {
-      return new Response("Bad request", { status: 400 });
-    }
-    // find by email and update
-    const { modifiedCount } = await usersCollection.updateOne(
-      { email: user.email },
-      { $set: { name: user.name, age: user.age } }
-    );
+    if (path === "/user") {
+      const user = await req.json();
+      if (!user.name || !user.email || !user.age || !user.books) {
+        return new Response("Bad request", { status: 400 });
+      }
 
-    if (modifiedCount === 0) {
-      return new Response("User not found", { status: 404 });
-    }
+      if (user.books) {
+        const books = await booksCollection
+          .find({
+            _id: { $in: user.books.map((id: string) => new ObjectId(id)) },
+          })
+          .toArray();
+        if (books.length !== user.books.length) {
+          return new Response("Book not found", { status: 404 });
+        }
+      }
 
-    return new Response("OK", { status: 200 });
+      // find by email and update
+      const { modifiedCount } = await usersCollection.updateOne(
+        { email: user.email },
+        { $set: { name: user.name, age: user.age, books: user.books } }
+      );
+
+      if (modifiedCount === 0) {
+        return new Response("User not found", { status: 404 });
+      }
+
+      return new Response("OK", { status: 200 });
+    } else if (path === "/book") {
+      const book = await req.json();
+      if (!book.id || !book.title || !book.pages) {
+        return new Response("Bad request", { status: 400 });
+      }
+
+      const { modifiedCount } = await booksCollection.updateOne(
+        { _id: new ObjectId(book.id as string) },
+        { $set: { title: book.title, pages: book.pages } }
+      );
+
+      if (modifiedCount === 0) {
+        return new Response("Book not found", { status: 404 });
+      }
+
+      return new Response("OK", { status: 200 });
+    }
   } else if (method === "DELETE") {
     const user = await req.json();
     if (!user.email) {
