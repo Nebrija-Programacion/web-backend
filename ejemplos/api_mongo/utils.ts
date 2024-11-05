9e420fa6340636a91735d7f86773f3c05672b1cd
Alberto Valero Gómez <alberto.valero@bqeducacion.cc>
2024-11-05 16:56:06 +0100

    ejemplo completo

--- ejemplos/api_mongo/utils.ts
+++ ejemplos/api_mongo/utils.ts
@@ -3,22 +3,18 @@ import type { BookModel, User, UserModel } from "./types.ts";
 import type { Book } from "./types.ts";
 
 export const fromModelToUser = async (
-  model: UserModel,
-  booksCollection: Collection<BookModel>,
+  userDB: UserModel,
+  booksCollection: Collection<BookModel>
 ): Promise<User> => {
   const books = await booksCollection
-    .find({ _id: { $in: model.books } })
+    .find({ _id: { $in: userDB.books } })
     .toArray();
   return {
-    id: model._id!.toString(),
-    name: model.name,
-    email: model.email,
-    age: model.age,
-    books: books.map((b) => ({
-      id: b._id.toString(),
-      title: b.title,
-      pages: b.pages,
-    })),
+    id: userDB._id!.toString(),
+    name: userDB.name,
+    email: userDB.email,
+    age: userDB.age,
+    books: books.map((b) => fromModelToBook(b)),
   };
 };
 
